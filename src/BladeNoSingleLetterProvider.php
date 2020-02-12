d77214ab67fe402b4fc0079f944902ce47c71c57
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2020-02-12 11:11:32 +0100

    Initial commit

---
+++ src/BladeNoSingleLetterProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LiquidPL\BladeNoSingleLetter;
+
+use Illuminate\Support\ServiceProvider;
+
+class BladeNoSingleLetterProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
