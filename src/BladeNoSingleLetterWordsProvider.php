c5d32607c9b020feddf9b43ee297ae932be92bed
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2020-02-12 12:21:50 +0100

    Change package name

--- src/BladeNoSingleLetterWordsProvider.php
+++ src/BladeNoSingleLetterWordsProvider.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace LiquidPL\BladeNoSingleLetter;
+namespace LiquidPL\BladeNoSingleLetterWords;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
-class BladeNoSingleLetterProvider extends ServiceProvider
+class BladeNoSingleLetterWordsProvider extends ServiceProvider
 {
     public function boot()
     {
-        Blade::directive('nosingleletter', function () {
+        Blade::directive('nosingleletterwords', function () {
             return '<?php ob_start() ?>';
         });
 
-        Blade::directive('endnosingleletter', function () {
+        Blade::directive('endnosingleletterwords', function () {
             return "<?php echo preg_replace('/(\s\S{1,2})\s(?![^<]*>)/m', '$1&nbsp;', ob_get_clean()) ?>";
         });
     }
