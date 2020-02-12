939f356c8b744e725d134a0e4ece77d5cda60a42
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2020-02-12 11:48:20 +0100

    Add blade directives

--- src/BladeNoSingleLetterProvider.php
+++ src/BladeNoSingleLetterProvider.php
@@ -2,12 +2,19 @@
 
 namespace LiquidPL\BladeNoSingleLetter;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class BladeNoSingleLetterProvider extends ServiceProvider
 {
     public function boot()
     {
+        Blade::directive('nosingleletter', function () {
+            return '<?php ob_start() ?>';
+        });
 
+        Blade::directive('endnosingleletter', function () {
+            return "<?php echo preg_replace('/(\s\S{1,2})\s(?![^<]>)/m', '$1&nbsp;', ob_get_clean()) ?>";
+        });
     }
 }
